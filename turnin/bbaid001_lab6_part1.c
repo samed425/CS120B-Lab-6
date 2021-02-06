bfce4693df91e14d4f6e26aab4eea38da3e99889
samed425 <77028650+samed425@users.noreply.github.com>
2021-02-05 22:26:11 -0800

    Update bbaid001_lab6_part1.c

--- turnin/bbaid001_lab6_part1.c
+++ turnin/bbaid001_lab6_part1.c
@@ -6,6 +6,8 @@
  *	
  *	*I acknowledge all content contained herein, excluding template or example
  *	*code, is my own original work.
+ *
+ *      Demo Link: https://www.youtube.com/watch?v=WQzCCZaqL98
  *	*/
 
 #include <avr/io.h>
